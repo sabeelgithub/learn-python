eee547629745104808edfd4406ffd44fb664f2bf
MOHAMMED SABEEL <mohammedsabeeltc786@gmail.com>
2023-10-12 12:00:44 +0530

    sorted() method is added

---
+++ sorted.py
@@ -0,0 +1,10 @@
+###########  sorted() ###########
+
+# sorted() method is using for sorting
+var = [1,5,9,0,6,5]
+res = sorted(var)
+print(res)
+# in decreasing order
+print(sorted([1,5,9,0,6,5],reverse=True))
+
+######### end ###############
