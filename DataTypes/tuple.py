79ebe833ada994c0607107a49f41365b43a4d585
mohammedsabeel <mohammedsabeeltc786@gmail.com>
2024-11-02 19:21:25 +0530

    tuple revised

--- DataTypes/tuple.py
+++ DataTypes/tuple.py
@@ -1,6 +1,10 @@
 ############# TUPLE ###############
+# tuple()
+# tuple can be create using tuple constructor by passing iterables
 thisTuple = tuple(('lo',1,False,'ann'))
 print(thisTuple)
+
+# accessing and slicing
 print(thisTuple[0])
 print(thisTuple[:])
 print(thisTuple[0:])
@@ -8,6 +12,7 @@ print(thisTuple[0:3])
 print(thisTuple[-3:-1])
 print(thisTuple[-3:])
 
+# tuple is immutable,cannot be changed after creation
 # in to list
 thisList = list(thisTuple)
 print(thisList)
@@ -19,6 +24,7 @@ print(thisTuple)
 into_set = set(thisTuple)
 print(into_set)
 
+# tuples can be added and multiplied
 tp1 = ('wq',1,True,None)
 tp2 = ('d','w')
 print(tp1+tp2)
